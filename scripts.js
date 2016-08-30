1a9f7f013bf0a740ca00060582bae933bc28cec0
Ryan Westlake <ryancwestlake@gmail.com>
2016-08-30 15:10:11 -0600

    Added div which holds input fields and read and remove buttons

--- scripts.js
+++ scripts.js
@@ -2,8 +2,12 @@ $( document ).ready(function() {
 
   $('.add-bookmark').on('click', function(){
     var title = $('.title-field').val();
+    var url = $('.url-field').val();
     console.log(title);
-    $('.bookmark-list').append('<li>' + title + '</li>');
+    console.log(url);
+    var bookmarkDiv = ('<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark">Remove Bookmarks</button></div>');
+
+    $('.bookmark-list').append(bookmarkDiv);
   });
 
 
