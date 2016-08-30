b7deb548fb39b0b76e07720bace07606dc58ff0a
Ryan Westlake <ryancwestlake@gmail.com>
2016-08-30 16:09:45 -0600

    Error message appears if input field is blank

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 $( document ).ready(function() {
 
+
   $('.add-bookmark').on('click', function(){
     var title = $('.title-field').val();
     var url = $('.url-field').val();
     console.log(title);
     console.log(url);
-    var bookmarkDiv = ('<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark-button">Remove Bookmarks</button></div>');
-
-    $('.bookmark-list').append(bookmarkDiv);
+    appendToSection();
   });
+
+  function appendToSection(title, url) {
+    if ($('.title-field').val() === '' || $('.url-field').val() === '') {
+      $('.error-message').text('Error: One of the fields is empty. Please fill in both fields.');
+    } else {
+    $('.bookmark-list').append(
+      '<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark-button">Remove Bookmarks</button></div>');
+    $('.error-message').text('');
+  }
+}
+
 
   $('.bookmark-list').on('click', '.read-button', function(){
     $(this).parent().toggleClass('read');
@@ -17,7 +27,5 @@ $( document ).ready(function() {
   $('.bookmark-list').on('click', '.remove-bookmark-button', function(){
     $(this).parent().remove();
   });
-
-
 
 });
