aa1005f34443f11cefb4a83784bf8136dd7af47f
Ryan Westlake <ryancwestlake@gmail.com>
2016-08-30 15:32:29 -0600

    added marked as read button, functionality work, adds read class to container

--- scripts.js
+++ scripts.js
@@ -5,10 +5,19 @@ $( document ).ready(function() {
     var url = $('.url-field').val();
     console.log(title);
     console.log(url);
-    var bookmarkDiv = ('<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark">Remove Bookmarks</button></div>');
+    var bookmarkDiv = ('<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark-button">Remove Bookmarks</button></div>');
 
     $('.bookmark-list').append(bookmarkDiv);
   });
+
+  $('.bookmark-list').on('click', '.read-button', function(){
+    $(this).parent().addClass('read');
+  });
+
+  $('.bookmark-list').on('click', '.remove-bookmark-button', function(){
+    alert('hello');
+  });
+
 
 
 });
