44e5d1511f82550b4dee521a13847c5e1dd8c957
Ryan Westlake <ryancwestlake@gmail.com>
2016-08-30 18:11:33 -0600

    bookmark counts are working

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ $( document ).ready(function() {
   $('.add-bookmark').on('click', function(){
     var title = $('.title-field').val();
     var url = $('.url-field').val();
-    console.log(title);
-    console.log(url);
     appendToSection(title, url);
+    changeNumberBookmarks();
+
   });
 
   $('.title-field').on('keyup', function() {
@@ -29,29 +29,24 @@ $( document ).ready(function() {
     }
   });
 
-  // function checkingInputFields() {
-  //   if ($('.title-field').val() === '' || $('.url-field').val() === '') {
-  //     $('.error-message').text('Error: One of the fields is empty. Please fill in both fields.');
-  //     $('.add-bookmark').prop('disabled', true);
-  //   } else {
-  //     $('.add-bookmark').prop('disabled', false);
-  //     $('.error-message').text('');
-  //   }
-  // }
-
-
   function appendToSection(title, url) {
     $('.bookmark-list').append(
       '<div class="container"><li class="title-style">' + title + '</li><li class="link-style">' + url + '</li><button class="read-button">Mark Read</button><button class="remove-bookmark-button">Remove Bookmarks</button></div>');
-    $('.error-message').text('');
   }
 
   $('.bookmark-list').on('click', '.read-button', function(){
     $(this).parent().toggleClass('read');
+    changeNumberBookmarks();
   });
 
   $('.bookmark-list').on('click', '.remove-bookmark-button', function(){
     $(this).parent().remove();
+    changeNumberBookmarks();
   });
 
+function changeNumberBookmarks() {
+  $('.total-bookmarks').text($('.container').length);
+  $('.read-bookmarks').text($('.read').length);
+  $('.unread-bookmarks').text($('.container').length - $('.read').length);
+}
 });
